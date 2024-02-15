73480cb7f74b8a4f5e31db8fe11cc5bcc5ed90fd
Robin Uhrich <robin.uhrich@gmail.com>
2024-02-15 11:58:29 +0100

    implemented 6 tests for basic types: int, str, float, bool

---
+++ test/__init__.py
@@ -0,0 +1,12 @@
+# define the test values
+
+STR = "foo"
+INT = 42
+FLOAT = 3.141
+BOOL_FALSE = False
+BOOL_TRUE = True
+EMPTY_LIST = []
+INT_LIST = [1, 2, 3]
+FLOAT_LIST = [4.5, 5.6, 6.7]
+STR_LIST = ["a", "b", "cd", "efg"]
+BOOL_LIST = [False, True, False, True]
---
+++ test/test_argument_writing.py
@@ -0,0 +1,9 @@
+
+
+import subprocess
+
+
+def test_terminal_api():
+    # call the test module just once with help just to have a look if the generating has worked out
+    cmd = "python -m test.test_project --help"
+    subprocess.run(cmd, shell=True, capture_output=True)
