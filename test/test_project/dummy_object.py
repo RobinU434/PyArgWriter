70cfd22df106a0278a34dc0ec2490e6f6aa44059
Robin Uhrich <robin.uhrich@gmail.com>
2024-02-15 22:27:32 +0100

    added new test case classes

---
+++ test/test_project/dummy_object.py
@@ -0,0 +1,10 @@
+class DummyClass:
+    """dummy class"""
+
+    def __init__(self) -> None:
+        """init dummy class"""
+        pass
+
+    def do_something(self):
+        """do something"""
+        return
---
+++ test/test_project/fail_class.py
@@ -0,0 +1,9 @@
+class FailClass:
+    """class to fail requirements"""
+
+    def __init__(self) -> None:
+        """init"""
+        pass
+
+    def fail_no_docstring(self):
+        return
